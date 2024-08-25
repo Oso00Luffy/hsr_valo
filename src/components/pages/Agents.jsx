8ee0a578ccf6003940e0cf4ed09032baf140dddf
Osama Jaradat <osojr2017@gmail.com>
2024-08-25 16:00:17 +0300

    Update Agents.jsx

--- src/components/pages/Agents.jsx
+++ src/components/pages/Agents.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles, { layout } from "../../style";
 import agent1Image from "../../assets/moze.png"; // Import the image
-import agent2Image from "../../assets/moze.jpg"; // Import more images as needed
+import agent2Image from "../../assets/moze.png"; // Import more images as needed
 
 const agentsData = [
   {
