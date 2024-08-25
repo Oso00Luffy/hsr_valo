e249b36b7d676fa81cb9931b2ae154e93d3b845f
Osama Jaradat <osojr2017@gmail.com>
2024-08-25 13:28:34 +0300

    changes

--- src/components/layout/CTA.jsx
+++ src/components/layout/CTA.jsx
@@ -4,10 +4,10 @@ const CTA = () =>(
     <div className="flex justify-center align-center bg-[#292929] p-8">
       <ul className="flex sm:flex-row flex-col justify-center align-center">
         <li className="font-poppins font-semibold transition-all ease-in duration-300 relative cursor-pointer text-center text-[13px] hover:bg-[gray]/40 px-4 py-2 rounded-[8px] uppercase mr-6 text-white">
-          <a href="#">DOWNLOAD GAME CLIENT</a>
+          <a href="https://hsr.hoyoverse.com/en-us/">حمل اللعبة</a>
         </li>
         <li className="font-poppins font-semibold transition-all ease-in duration-300 relative cursor-pointer text-center text-[13px] hover:bg-[gray]/40 px-4 py-2 rounded-[8px] uppercase mr-6 text-white">
-          <a href="#">DOWNLOAD RIOT MOBILE COMPANION APP</a>
+          <a href="https://hsr.hoyoverse.com/en-us/">حمل اللانشر الخاص بها</a>
         </li>
       </ul>
     </div>
