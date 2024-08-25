3312e9ad14df8e454247f91e6a5afc8c6d90036c
Osama Jaradat <osojr2017@gmail.com>
2024-08-25 14:15:18 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,23 +32,23 @@ export const news = [
   {
     id: "news-1",
     title: "AGENT COSPLAY GUIDE: HARBOR",
-    date: "10/27/22",
-    category: "COMMUNITY",
-    imglink: "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltefa41c5a9c334a87/6356e33edc15735d5cd03980/Val_Banner_Harbor_Cosplay_1920x1080_New_Tattoo.jpg?auto=webp&disable=upscale&width=1073"
+    date: "25/8/24",
+    category: "المجتمع",
+    imglink: "https://cdn.discordapp.com/attachments/623877027086336010/1277223256385912883/photo_2024-08-25_14-09-01.jpg?ex=66cc6259&is=66cb10d9&hm=2767225457768f234fa7b259e9818a7013733e43d81bfc44445203571287dd89&"
   },
   {
     id: "news-2",
-    title: "VALORANT’S PREMIER ALPHA TEST",
-    date: "10/26/22",
-    category: "ANNOUNCEMENTS",
-    imglink: "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt916477036f3b64c7/63574eaf9a123e5dbdbc61d5/PremierAlpha_Announce_Banner16x9_optimized.jpg?auto=webp&disable=upscale&width=1073"
+    title: "لايف ريآكشن - كرستيانو و جورجينا: ستار ريل افضل لعبة",
+    date: "25/8/24",
+    category: "الإعلانات",
+    imglink: "https://cdn.discordapp.com/attachments/623877027086336010/1277223628701696052/photo_2024-08-25_14-10-32.jpg?ex=66cc62b2&is=66cb1132&hm=27e59c3d13f2597d52831796959f2c34d14ea947f08cce9f3d97caba68024bf3&"
   },
   {
     id: "news-3",
-    title: "WHAT’S NEW IN VALORANT EPISODE 5 ACT III",
-    date: "10/18/22",
-    category: "GAME UPDATES",
-    imglink: "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltea3dd93cc82f3b59/63484defe9d2c541844df655/VALORANT_2022_EP5-3_Article_Banner_1920x1080_MB01.jpg?auto=webp&disable=upscale&width=1073"
+    title: "ما هو الجديد في هونكاي ستار ريل",
+    date: "25/8/24",
+    category: "تحديثات اللعبة",
+    imglink: "https://cdn.discordapp.com/attachments/623877027086336010/1277224064016060437/photo_2024-08-25_14-12-10.jpg?ex=66cc631a&is=66cb119a&hm=4c3c9ced425c0fb20277566cf9f9b47280e24a988b13ff79531f182aa739d0ae&"
   },
 ]
 
